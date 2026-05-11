cdfd1ec105fceafdf241787b0598daee92b84df5
Dhruv Gupta <dhruv06012@gmail.com>
2026-05-11 22:57:13 +0530

    add the render delpoy link on streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ import requests
 import streamlit as st
 
 
-API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000").rstrip("/")
+API_BASE_URL = os.getenv(
+    "API_BASE_URL",
+    "https://assessment-recommender-nzl3.onrender.com"
+).rstrip("/")
 REQUEST_TIMEOUT_SECONDS = 30
 
 
